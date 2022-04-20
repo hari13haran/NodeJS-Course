76ac8a9de8291d285bd2e399fce56fcd62381eb9
hari13haran <h.hariran@gmail.com>
2022-04-21 00:57:14 +0530

    OS file import & access

--- module.js
+++ module.js
@@ -3,3 +3,7 @@ const { people, age } = require('./people');
 
 console.log(people);
 console.log(age);
+
+// to import the built-in OS file & fetch details about it
+const os = require('os');
+console.log(os.platform(), os.homedir());
