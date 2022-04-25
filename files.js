4a854d5dba3d98be7be7bb4d4229c4c8eb7183e0
hari13haran <h.hariran@gmail.com>
2022-04-26 00:18:13 +0530

    delete files

--- files.js
+++ files.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+//read file
 fs.readFile('./docs/blog1.txt', (err, data) => {
     if(err) {
         console.log(err);
@@ -7,9 +8,41 @@ fs.readFile('./docs/blog1.txt', (err, data) => {
     console.log(data.toString());
 });
 
-console.log("This is the last line of code");
+//console.log("This is the last line of code");
 
+//write file
 //overrites if its existing file & creates a new file if it doesnt exist
 fs.writeFile('./docs/blog2.txt','Hello world! new file created!', () => {
     console.log('File created & written sucessfully')
 });
+
+// create and delete folders / directories
+if(!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Folder created!');
+    });
+}
+else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('Folder deleted!');
+    });
+}
+
+//delete files
+if(fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink ('./docs/deleteme.txt' , (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('File deleted!')
+    });
+}
+else {
+    console.log('deleteme.txt file doesnt exist. pls create one!');
+}
