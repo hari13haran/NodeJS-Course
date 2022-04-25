7c501a4b1c5b861864c6f476e43fdf4430da6777
hari13haran <h.hariran@gmail.com>
2022-04-25 23:58:22 +0530

    Write & create files

--- files.js
+++ files.js
@@ -8,3 +8,8 @@ fs.readFile('./docs/blog1.txt', (err, data) => {
 });
 
 console.log("This is the last line of code");
+
+//overrites if its existing file & creates a new file if it doesnt exist
+fs.writeFile('./docs/blog2.txt','Hello world! new file created!', () => {
+    console.log('File created & written sucessfully')
+});
