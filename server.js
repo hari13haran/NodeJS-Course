124ecf7b731b894ef04389ca882dc428a110ad0b
hari13haran <h.hariran@gmail.com>
2022-05-21 00:41:50 +0530

    Simple URL Routing

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ const server = http.createServer((req,res) => {
     // Send a html file as response
     res.setHeader('Content-Type', 'text/html');
 
-    fs.readFile('./views/index.html', (err, data) => {
+    // Simple URL Routing
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err);
             res.end();
