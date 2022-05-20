94dac20130938fa52faee064286a657303502455
hari13haran <h.hariran@gmail.com>
2022-05-21 00:09:26 +0530

    returning text as response

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log("Request made!");
+    //console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello! response sent!');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
