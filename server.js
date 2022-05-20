612fde776b21e5424ef8817c5da3df6fb04bfad2
hari13haran <h.hariran@gmail.com>
2022-05-21 00:28:01 +0530

    send html page as response

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     //console.log(req.url, req.method);
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('Hello! response sent!');
-    res.end();
+    // Send a text response
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.write('Hello! response sent!');
+    // res.end();
+
+    // Send a html file as response
+    res.setHeader('Content-Type', 'text/html');
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+        }
+    });
+
 });
 
 server.listen(3000, 'localhost', () => {
