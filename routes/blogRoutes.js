09169d7c6f83fa0218af7d8cf0b82491d6d7a071
hari13haran <h.hariran@gmail.com>
2022-06-20 23:29:10 +0530

    Adapted MVC model structure

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,59 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 
 // GET request to fetch all blogs from DB & display on index page
-router.get('/', (req,res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All blogs', blogs: result})
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+router.get('/', blogController.blog_index);
 
 // POST request to save blogs in DB
-router.post('/', (req,res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+router.post('/', blogController.blog_create_post);
 
 // GET request to redirect to the create blog view
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
+router.get('/create', blogController.blog_create);
 
 // GET request to fetch single blog with _id
-router.get('/:id', (req,res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { title:'Blog Details', blog: result});
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+router.get('/:id', blogController.blog_details);
 
 // DELETE request to delete a single blog with _id
-router.delete('/:id', (req,res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            // the redirect needs to be handled in browser since its an AJAX request
-            res.json({ redirect: '/blogs' });
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-});
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
